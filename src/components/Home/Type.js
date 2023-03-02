1dc9f2c51b3db06e5af6484e61a7eee4aac88038
Mehmet veysi özek <114666552+mehmetozek01@users.noreply.github.com>
2023-03-02 15:07:04 +0300

    d

---
+++ src/components/Home/Type.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+function Type() {
+  return (
+    <Typewriter
+      options={{
+        strings: [
+          "Developer",
+          "Front End Developer",
+          "Mobile App Developer",
+          // "Open Source Contributor",
+        ],
+        autoStart: true,
+        loop: true,
+        deleteSpeed: 50,
+      }}
+    />
+  );
+}
+
+export default Type;
